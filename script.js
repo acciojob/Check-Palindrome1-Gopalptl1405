3c8d65161d5234ded75f682b34ef1c48db7bcdae
Gopal Patel <130164161+Gopalptl1405@users.noreply.github.com>
2023-05-16 11:27:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
-
+     let new_str = str.toLowerCase();
+	let a =0;
+	let b= new_str.length()-1;
+	while(a<b)
+		{
+			if(new_str.charAt(a) != new_str.charAt(b)){
+				return false;
+			}
+			a++;
+			b--;
+		}
+	return true;
 }
 module.exports = palindrome
